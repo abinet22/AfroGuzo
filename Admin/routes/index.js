f2543674bc8adb0a4bde73908de384b937ec9d24
Abinet Tefera <abinet22@gmail.com>
2022-04-20 23:44:17 +0300

    balbla

--- Admin/routes/index.js
+++ Admin/routes/index.js
@@ -9,6 +9,9 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 router.get('/', forwardAuthenticated, (req, res) => res.render('login'));
 router.get('/dashboard', forwardAuthenticated, (req, res) => res.render('dashboard'));
 router.get('/newapplication', forwardAuthenticated, (req, res) => res.render('applicationform'));
+router.get('/applicantdatalist', forwardAuthenticated, (req, res) => res.render('applicantdata'));
+
+router.get('/statisticsandmore', forwardAuthenticated, (req, res) => res.render('statisticsandmore'));
 
 router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
 module.exports = router;
